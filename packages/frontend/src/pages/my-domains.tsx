8effbda0c31b7f7ec148b9a48c80761766697019
Felix Vemmer <felix.vemmer@gmail.com>
2022-09-03 14:45:18 +0200

    feat: adds Domain Overview

--- packages/frontend/src/pages/my-domains.tsx
+++ packages/frontend/src/pages/my-domains.tsx
@@ -1,7 +1,56 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import { Button, Table } from 'react-daisyui'
+
+const myDomains = [
+  {
+    id: 1,
+    name: 'test',
+    address: '0x123',
+  },
+  {
+    id: 2,
+    name: 'test2',
+    address: '0x123',
+  },
+]
 
 const MyDomains: NextPage = () => {
-  return <p>My Domains</p>
+  return (
+    <div className="flex flex-col items-center justify-center overflow-x-auto">
+      <h1 className="pb-12 text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-secondary-content to-error">
+        Domain Overview
+      </h1>
+      <Table>
+        <Table.Head>
+          <span />
+          <span>Domains</span>
+          <span>Adress</span>
+          <span>Actions</span>
+        </Table.Head>
+
+        <Table.Body>
+          {myDomains.map((domain, index) => (
+            <Table.Row key={domain.id} hover>
+              <span>{index + 1}</span>
+              <span>{domain.name}</span>
+              <span>{domain.address}</span>
+              <span>
+                <div className="flex space-x-4">
+                  <Link href={`my-domains/${domain.id}`}>
+                    <a className="btn btn-outline btn-sm">Manage</a>
+                  </Link>
+                  <Button color="error" variant="outline" size="sm">
+                    Release Domain
+                  </Button>
+                </div>
+              </span>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </div>
+  )
 }
 
 export default MyDomains

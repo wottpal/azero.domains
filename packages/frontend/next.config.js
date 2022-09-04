6430859ca755f6caa08250f6b028636db8b89244
Dennis Zoma <3930150+wottpal@users.noreply.github.com>
2022-09-04 15:51:42 +0200

    frontend: Fix deployment config

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -5,8 +5,8 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  trailingSlash: true,
-  ...(process?.env?.NODE_ENV === 'production' ? { assetPrefix: './' } : {}),
+  // trailingSlash: true,
+  // ...(process?.env?.NODE_ENV === 'production' ? { assetPrefix: './' } : {}),
 }
 
 module.exports = nextConfig
